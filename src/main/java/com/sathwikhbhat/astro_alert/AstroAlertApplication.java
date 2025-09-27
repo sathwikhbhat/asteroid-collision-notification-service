a925cf7b0af614d2e971c79e8a48ab24cce07317
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-09-27 18:17:04 +0530

    chore: initialize project structure with Maven and Spring Boot

---
+++ src/main/java/com/sathwikhbhat/astro_alert/AstroAlertApplication.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.astro_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sathwikhbhat/astro_alert/AstroAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.astro_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
