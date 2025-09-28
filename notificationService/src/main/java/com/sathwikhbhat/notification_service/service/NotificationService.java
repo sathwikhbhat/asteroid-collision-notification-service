a6ce1a291bcd99366723e1472a8b8a1ecd9a607c
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-09-28 13:08:38 +0530

    feat: implement alert event handling in NotificationService

--- notificationService/src/main/java/com/sathwikhbhat/notification_service/service/NotificationService.java
+++ notificationService/src/main/java/com/sathwikhbhat/notification_service/service/NotificationService.java
@@ -1,17 +1,37 @@
 package com.sathwikhbhat.notification_service.service;
 
 import com.sathwikhbhat.astro_alert.event.AsteroidCollisionEvent;
+import com.sathwikhbhat.notification_service.entity.Notification;
+import com.sathwikhbhat.notification_service.repository.NotificationRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Service
 @Slf4j
 public class NotificationService {
 
+    @Autowired
+    NotificationRepository notificationRepository;
+
     @KafkaListener(topics = "asteroid-alert", groupId = "notification-service")
     public void alertEvent(AsteroidCollisionEvent notificationEvent) {
         log.info("Received Asteroid alerting event: {}", notificationEvent);
+
+        Notification notification = Notification.builder()
+                .asteroidName(notificationEvent.getAsteroidName())
+                .closeApproachDate(LocalDate.parse(notificationEvent.getCloseApproachDate()))
+                .estimatedDiameterAvgMeters(notificationEvent.getEstimatedAvgDiameterInMeters())
+                .missDistanceKilometers(new BigDecimal(notificationEvent.getMissDistanceInKm()))
+                .emailSent(false)
+                .build();
+
+        Notification savedNotification = notificationRepository.saveAndFlush(notification);
+        log.info("Notification saved: {}", savedNotification);
     }
 
 }
